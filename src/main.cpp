d3d0b4bdf7e4e46d8658670b67e4761b2d4a22b8
robos <robosam2003@gmail.com>
2022-11-21 13:43:45 +0000

    fix cartesianToSpherical

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,10 @@ double currentAngle1 = 0;
 double currentAngle2 = 0;
 double currentAngle3 = 0;
 
+double armLengths[3] = {115, 80, 130};
+double armAngles[3] = {0};
+
+
 #define increment 0.017 // radians per 15ms
 
 
@@ -51,62 +55,55 @@ double rateLimit(double desiredAngle, double &currentAngle) {
 }
 
 void cartesianToSpherical(double coords[3], double lengths[3], double angles[3]) {
-    double x = coords[0], y = coords[1]-lengths[0], z = coords[2];
-    double l = sqrt(x*x + y*y + z*z);
-    angles[2] = PI - acos( (l*l - lengths[2]*lengths[2] - lengths[1]*lengths[1]) / (-2*lengths[1]*lengths[2]) );
+    double l0 = lengths[0];
+    double l1 = lengths[1];
+    double l2 = lengths[2]; // 115, 80, 130
+    double x = coords[0];
+    double y = coords[1];
+    double z = coords[2];
 
+    // Length from base to end effector
+    double l = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+
+    // Base angle
+    angles[0] = atan2(z, x);
+
+    double theta2_prime = acos( (l1*l1 + l2*l2 - l*l) / (2*l1*l2) );
     double a = atan2(y, x);
-    double b = asin( (sin(angles[2])*lengths[2]) / l );
+    double b = asin( (sin(theta2_prime)*lengths[2]) / l );
     angles[1] = a + b;
-    //angles[0] = atan2(z, x);
+    angles[2] = -(PI - theta2_prime);
+
+    Serial.println(l);
+    Serial.println(angles[0]);
+    Serial.println(angles[1]);
+    Serial.println(angles[2]);
+    Serial.println();
 }
 
-double lengths[3] = {115, 80, 130};
-double angles[3] = {0};
 
 
 void goToPosition(double coords[3]) {
-    elapsedTime = millis() - timestamp;
-    if (elapsedTime >= controlRate) {
-        cartesianToSpherical(coords, lengths, angles);
-        S1.write(angles[0] * RAD_TO_DEG);
-        S2.write(angles[1] * RAD_TO_DEG);
-        S3.write(angles[2] * RAD_TO_DEG);
+        cartesianToSpherical(coords, armLengths, armAngles);
 
-//        int pot1 = analogRead(POT_PIN_1);
-//        int pot2 = analogRead(POT_PIN_2);
-//        int pot3 = analogRead(POT_PIN_3);
-//        double desiredAngle1 = floatMap(pot1, 0, 1023, -PI / 2, PI / 2);
-//        double desiredAngle2 = floatMap(pot2, 0, 1023, -PI / 2, PI / 2);
-//        double desiredAngle3 = floatMap(pot3, 0, 1023, -PI / 2, PI / 2);
 
         // print the desired angles
-        Serial.print("Desired Angle1: ");
-        Serial.print(angles[0]);
-        Serial.print(" Desired Angle2: ");
-        Serial.print(angles[1]);
-        Serial.print(" Desired Angle3: ");
-        Serial.println(angles[2]);
+//        Serial.print("Desired Angle1: ");
+//        Serial.print(angles[0]);
+//        Serial.print(" Desired Angle2: ");
+//        Serial.print(angles[1]);
+//        Serial.print(" Desired Angle3: ");
+//        Serial.println(angles[2]);
 
-        // position limiting for safety
-        if (angles[1] < -0.9) {
-            angles[1] = -0.9;
-        }
-        double theta3Max = 1.9 * currentAngle2 + 0.79;
-        if (angles[2] > theta3Max) {
-            angles[2] = theta3Max;
-        }
-
-        int servoPulseWidth1 = floatMap(rateLimit(angles[0], currentAngle1), -PI / 2, PI / 2, 500, 2500);
-        int servoPulseWidth2 = floatMap(rateLimit(angles[2], currentAngle2), -PI / 2, PI / 2, 500, 2500);
-        int servoPulseWidth3 = floatMap(angles[2], -PI / 2, PI / 2, 500, 2500);
-
+        int servoPulseWidth1 = floatMap(armAngles[0], -PI / 2, PI / 2, 500, 2500);
+        int servoPulseWidth2 = floatMap(armAngles[1], -PI / 2, PI / 2, 500, 2500);
+        int servoPulseWidth3 = floatMap(armAngles[2], -PI / 2, PI / 2, 500, 2500);
 
         S1.writeMicroseconds(servoPulseWidth1);
         S2.writeMicroseconds(servoPulseWidth2);
         S3.writeMicroseconds(servoPulseWidth3);
 
-    }
+
 }
 
 void setup() {
@@ -117,16 +114,13 @@ void setup() {
 
     Serial.begin(9600);
 
-    pinMode(POT_PIN_1, INPUT);
-    pinMode(POT_PIN_2, INPUT);
-    pinMode(POT_PIN_3, INPUT);
+
 }
 
 double coords[3] = {120, 120, 0};
 //  Lab 2
 void loop() {
     goToPosition(coords);
-    delay(100);
 }
 
 
